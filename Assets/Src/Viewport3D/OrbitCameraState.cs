255ce82e99a2942da2f10dd4ae7502a7a7aa9a79
Petr Sevostianov <ps@antilatency.com>
2025-12-04 12:32:26 +0200

    *

--- Assets/Src/Viewport3D/OrbitCameraState.cs
+++ Assets/Src/Viewport3D/OrbitCameraState.cs
@@ -7,7 +7,7 @@ using UnityEngine.EventSystems;
 namespace FUI {
     [System.Serializable]
     public class OrbitCameraState : CameraState {
-        public float FOV = 45f;
+        public float TargetObjectRadius = 1f;
         public Vector3 Target;
         public float Distance = 5f;
         public int IntScale = 0;
@@ -29,16 +29,21 @@ namespace FUI {
             var rotation = Rotation;
             camera.transform.position = Target - (rotation * Vector3.forward * d);
             camera.transform.rotation = rotation;
-            camera.fieldOfView = FOV;
+
+            var targetObjectHalfHeight = (ViewportSize.y > ViewportSize.x)
+            ? TargetObjectRadius * (ViewportSize.y / ViewportSize.x)
+            : TargetObjectRadius;
+            var fov = 2f * Mathf.Atan2(targetObjectHalfHeight, Distance) * Mathf.Rad2Deg;
+            camera.fieldOfView = fov;
         }
         public override void HandleDrag(PointerEventData eventData) {
             var delta = eventData.delta;
             bool middleButton = eventData.button == PointerEventData.InputButton.Middle;
             if (middleButton) { // Pan
-                var distance = DistanceScaled;
-                var heightAtDistance = 2f * distance * Mathf.Tan(0.5f * FOV * Mathf.Deg2Rad);
-                
-                float panSpeed = heightAtDistance / ViewportSize.y; // world units per pixel
+
+                var objectRadiusAtDistance = TargetObjectRadius * ZoomFactor;
+                var minViewportSize = Mathf.Min(ViewportSize.x, ViewportSize.y);
+                float panSpeed = 2 * objectRadiusAtDistance / minViewportSize; // world units per pixel
                 var rotation = Rotation;
                 var right = rotation * Vector3.right;
                 var up = rotation * Vector3.up;
